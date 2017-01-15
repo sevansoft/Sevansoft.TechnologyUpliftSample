0acabe553a9dbdc8c9229e200f5b2823f409464c
Sevansoft <github@sevansoft.co.uk>
2017-01-15 12:06:56 +0000

    Added VS2015 project bound to 4.6.1

---
+++ VS2015/Sevansoft.TechnologyUpliftSample.Tests/PersonTests.vb
@@ -0,0 +1,28 @@
+﻿#Region "Header Block"
+Option Explicit On
+Option Strict On
+#End Region
+
+Imports System
+
+Imports NUnit.Framework
+
+Imports Sevansoft.TechnologyUpliftSample
+
+Namespace Sevansoft.TechnologyUpliftSample.Tests
+    <TestFixture()>
+    Public Class PersonTests
+        <Test()>
+        Public Sub ContructorTest()
+            Dim person As Person = New Person()
+
+            Assert.AreEqual(String.Empty, person.Firstname, "Firstname is invalid")
+            Assert.AreEqual(String.Empty, person.Surname, "Firstname is invalid")
+
+            person = New Person("Michael", "Mouse")
+
+            Assert.AreEqual("Michael", person.Firstname, "Firstname is invalid")
+            Assert.AreEqual("Mouse", person.Surname, "Firstname is invalid")
+        End Sub
+    End Class
+End Namespace
---
+++ VS2015/Sevansoft.TechnologyUpliftSample/Person.vb
@@ -0,0 +1,38 @@
+﻿#Region "Header Block"
+Option Explicit On
+Option Strict On
+#End Region
+
+Imports System
+Imports System.Diagnostics
+
+Namespace Sevansoft.TechnologyUpliftSample
+    <DebuggerStepThrough()>
+    Public Class Person
+        Public Sub New()
+            Me.New(String.Empty, String.Empty)
+        End Sub
+
+        Public Sub New(ByVal firstname As String,
+                       ByVal surname As String)
+            _firstname = firstname
+            _surname = surname
+        End Sub
+
+        Public ReadOnly Property Firstname As String
+            Get
+                Return _firstname
+            End Get
+        End Property
+
+        Public ReadOnly Property Surname As String
+
+            Get
+                Return _surname
+            End Get
+        End Property
+
+        Private _firstname As String
+        Private _surname As String
+    End Class
+End Namespace
